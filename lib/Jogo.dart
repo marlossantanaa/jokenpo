da2d8d252fcbac15acac43ba0da8a6fb38d959b1
Marlos Santana <83357882+marlossantanaa@users.noreply.github.com>
2022-09-28 18:33:31 -0300

    Pedra, papel e tesoura jogo de mãos recreativo e simples .

--- lib/Jogo.dart
+++ lib/Jogo.dart
@@ -1,12 +1,71 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 class Jogo extends StatefulWidget {
-  const Jogo({Key? key}) : super(key: key);
 
   @override
   State<Jogo> createState() => JogoState();
 }
 
 class JogoState extends State<Jogo> {
+
+  var _imgApp=AssetImage("images/padrao.png");
+  var _mensagem ="Escolha uma opção abaixo :)";
+
+ void _opcaoSelecionada(String escolhaUsuario) {
+
+   var opcoes = ["pedra", "papel", "tesoura"];
+   var numero = Random().nextInt(3);
+   var escolhaApp = opcoes[numero];
+
+   print ("Escolha do App :" + escolhaApp);
+   print("Escolha do Usuario:"+escolhaUsuario);
+
+//exibiçao APP
+
+ switch(escolhaApp){
+   case "pedra":
+     setState(() {
+       this._imgApp=AssetImage("images/pedra.png");
+     });
+     break;
+
+     case "papel":
+     setState(() {
+       this._imgApp=AssetImage("images/papel.png");
+     });
+       break;
+
+   case "tesoura":
+     this._imgApp=AssetImage("images/tesoura.png");
+     break;
+ }
+   //validaçao do Ganhador
+
+   //Usuario Ganhador
+   if(
+   (escolhaUsuario ==" pedra" && escolhaApp =="tesoura") ||
+   (escolhaUsuario == " tesoura" && escolhaApp =="papel")||
+   (escolhaUsuario=="papel" && escolhaApp =="pedra"))
+   {
+     setState(() {
+       this._mensagem="Parabéns!!! Você Ganhou :)";
+     });
+
+     //App Ganhador
+   }else if(
+      (escolhaApp =="pedra" && escolhaUsuario =="tesoura") ||
+          (escolhaApp == "tesoura " && escolhaUsuario =="papel") ||
+       (escolhaApp == "papel "&& escolhaUsuario == "pedra")
+   ){
+     this._mensagem="Que pena Você perdeu :(";
+
+    //Empate
+   }else{
+           this._mensagem="Empatamos ;)";
+   }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +77,7 @@ class JogoState extends State<Jogo> {
 body:Column(
   crossAxisAlignment: CrossAxisAlignment.center,
   children: <Widget>[
+
     Padding(
       padding: EdgeInsets.only(top:35,bottom: 10),
       child: Text(
@@ -26,29 +86,40 @@ body:Column(
         style: TextStyle(
           fontSize:30 ,
           fontWeight:FontWeight.bold ,
-                  ),
 
+        ),
       ),
     ),
-    Image.asset("images/padrao.png",height: 200),
+
+Image(image: this._imgApp,),
     Padding(
       padding: EdgeInsets.only(top:32,bottom: 16 ),
       child:Text(
-        "Escolha uma opção abaixo :)",
+        this._mensagem,
         textAlign:TextAlign.center,
         style: TextStyle(
           fontSize: 24,
           fontWeight:FontWeight.bold,
-                  ),
+        ),
       )
-          ),
+    ),
+
 
     Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              Image.asset("images/pedra.png",height: 100),
-              Image.asset("images/papel.png",height: 100),
-              Image.asset("images/tesoura.png",height: 100)
+              GestureDetector(
+                onTap:()=> _opcaoSelecionada("pedra"),
+                child: Image.asset("images/pedra.png",height: 100),
+              ),
+              GestureDetector(
+                onTap: ()=> _opcaoSelecionada("papel"),
+                child: Image.asset("images/papel.png",height: 100),
+              ),
+              GestureDetector(
+                onTap:()=> _opcaoSelecionada("tesoura"),
+                child:  Image.asset("images/tesoura.png",height: 100),
+              )
 
             ],
           ),
